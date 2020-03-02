3c1b63a6dbc4d2d6f1e357ac3d94f4f821fb01cc
Willis Aybar <willisaybar@gmail.com>
2020-03-02 23:45:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,28 +8,29 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs << Song
+  # def songs
+  #   @songs << Song
+  # end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
   end
 
-  # def add_song(song)
-  #   self.songs << song
-  #   song.artist = self
-  #   @@song_count +=1
-  # end
-  #
-  # def add_song_by_name(name)
-  #   song = Song.new(name)
-  #   self.songs << song
-  #   song.artist = self
-  #   @@song_count +=1
-  # end
-  #
-  # def songs
-  #   @songs
-  # end
-  #
-  # def self.song_count
-  #   @@song_count
-  # end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
 end
